f8571a4afbae3aeeac4d55f86bd518f56505f7f5
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-03-03 14:17:51 +0300

    136_Ending_Course

--- petclinic_hibernate/src/main/java/com/javaegitimleri/petclinic/entity/Visit.java
+++ petclinic_hibernate/src/main/java/com/javaegitimleri/petclinic/entity/Visit.java
@@ -1,9 +1,7 @@
 package com.javaegitimleri.petclinic.entity;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Entity
 @Table(name = "hsr_visit ")
